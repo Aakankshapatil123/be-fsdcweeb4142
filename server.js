86cb7a735b6fad4e2f06421c7442bc712f2b73f8
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-06-22 17:16:30 +0530

    request-response cycle --response

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
-// import http module;
+// import http module
 const http = require('http');
 
-// create http server
+// create a http server
 const server = http.createServer((request, response) => {
-response.write("hello nodejs!");
-response.end();
+  // response.setHeader('Content-Type', 'application/json');
+  // response.writeHead(201, { 'Content-Type': 'text/plain' });
+  // response.writeHead(200, { 'Content-Type': 'application/json' });
+  response.writeHead(200, { 'Content-Type': 'text/html' });
+  response.write(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>NODEJS</title>
+      </head>
+
+      <body>
+        <div>
+          <h1>Hello World!</h1>
+          <p>Welcome</p>
+        </div>
+      </body>
+    </hml>
+    `);
+  response.end();
 });
 
-// run http server
-server.listen(3001, 'localhost', () =>{
-    console.log('server is runnig @ http://localhost:3001');
+// run the http server
+server.listen(3001, 'localhost', () => {
+  console.log('server is running @ http://localhost:3001');
 });
