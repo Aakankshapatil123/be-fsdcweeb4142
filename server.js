7850419b273fbc5ee2dea36a5794c5eece3bddf8
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-06-24 12:03:27 +0530

    middlewares and custom middleware

--- server.js
+++ server.js
@@ -5,7 +5,16 @@ const express = require("express");
 // create an express application
 const app = express()
 
-// confihure the routes
+// middleware that acts like an oprator that logas all the request to the console or log file
+const logger = (request, response, next) => {
+ console.log(' i m a middleware');
+ next(); //calss the next middleware
+}
+
+// use the middleware
+app.use(logger);
+
+// confihure the routes for root route '/
 app.get('/', (request, response) => {
 response.json({message: 'hello GET!'})
 })
@@ -21,6 +30,18 @@ response.json({message: 'hello PUT!'})
 app.delete('/', (request, response) => {
 response.json({message: 'hello DELETE!'})
 })
+
+// configure the routes for test endpoint '/'
+app.get('/products', (request, response) => {
+response.json({message: 'hello get fot products!'})
+})
+
+// handle the error routes
+const errorRout = (request, response, next) => {
+response.json({message: "Route not found"})
+}
+
+app.use(errorRout)
 
 // start the server to losten for http requests
 app.listen(3001, 'localhost', (error) =>  {
