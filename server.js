5486d6e0fe4c2eee13ef9153603aa842149cd61e
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-06-25 16:47:33 +0530

    mongodb database connection --dene

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const app = require("./app");
+const mongoose = require('mongoose');
+require('dotenv').config();
 
+mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => {
+        console.log('Connected to the MongoDB Database');
 
+        // start the server to listen for http requests
+        app
+        .listen(3001, 'localhost', (error) => {
+            if (error) {
+                console.log('Error starting the server:', error.message);
+                return; // exits the function immediately if there is an error
+            }
 
-// start the server to losten for http requests
-app.listen(3001, 'localhost', (error) =>  {
-   if(error){
-     console.log('Enter starting the server')
-     console.log(error)
-     return //exit the function immediatly if there is an error
-   }
-
-    console.log('Server is runnig http://localhost:3001')
-});
+            console.log('Server is running at http://localhost:3001')
+        });
+    })
+    .catch((error) => {
+        console.log('Error connecting to the MongoDB Database:', error.message);
+    })
