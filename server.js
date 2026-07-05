315083556f7365ca1488b8ff0fc4e1a0fe9097b0
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-07-05 20:41:49 +0530

    Rename files

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const app = require("./app");
 const mongoose = require('mongoose');
 
-const {MONGODB_URL,PORT, HOST } = require("./utils/config")
+// const {MONGODB_URL,PORT, HOST } = require("./utils/config")
+const { MONGODB_URL, PORT, HOST} = require("./utils/config")
 
 
 mongoose
