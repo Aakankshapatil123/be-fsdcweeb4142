3a8b667825367eb54d73bbc1289d991c9d264672
Aakankshapatil123 <aakankshapatil6580@gmail.com>
2026-07-09 13:55:37 +0530

    register, login, me endpoints --done

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,6 @@
 const User = require("../models/user")
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken')
 
 const authController = {
     register: async (request, response) => {
@@ -6,13 +8,27 @@ const authController = {
             // get deails from the request body
             const { name, email, password } = request.body
 
+            // encrypt the password using bcrypt
+            const hashedPaassword = await bcrypt.hash(password, 10)
+
 
             // create the new object of the request body
              const newUser = new User({
                 name,
                 email,
-                password
+                password: hashedPaassword
              })
+
+            // check if the user already exists in the database
+            const existingUser = await User.findOne({ email }) 
+
+
+            // existingUser - null or an oject
+            // existingUser = null => user dose not exit
+            // existingUser = object => user already exists
+             if(existingUser){
+                return response.json({message: "user already exists!"})
+             }
 
             // strore the user in database
              await newUser.save();
@@ -21,6 +37,62 @@ const authController = {
 
         }catch(e) {
          return response.status(500).json({message: "Error register new user .try again later!", error:e.message})   
+        }
+    },
+
+    login: async (request, response) => {
+        try {
+            // get the datials from the request body
+            const { email, password } = request.body;
+            
+            // check user is exists in the database
+            const existingUser = await User.findOne({ email })
+
+            // if the user dose not exist, return error message
+            if(!existingUser){
+                return response.json({message: "user dose not exists!"})
+            }
+
+            // if we reach here, it means the user exit in the database
+            // compare the password provided by the user with the hashed password store in the database
+            const isPasswordValid = await bcrypt.compare(password, existingUser.password); 
+
+            // if the password is invalid , return error massage
+            if(!isPasswordValid){
+                return response.json({message: "Invalide password!"})
+            }
+            
+            // if we reach here , it meanse the user has provide valid credentials
+            // generate a jwt token for the user
+            const token = await jwt.sign({userId: existingUser._id}, 'apple', {expiresIn: '1h'})
+
+            // console.log(token)
+
+            response.status(200).json({message: "User login successfuly", token})
+
+        }catch(e) {
+         return response.status(500).json({message: "Error loggong in .try again later!", error:e.message})   
+        }
+    },
+
+    me: async (request, response) => {
+        try{
+            // get the token from the request headers
+            const token = request.headers.authorization.split(' ')[1];
+            
+            // verify the token
+            const decodedToken = await jwt.verify(token, 'apple')
+
+            // get the user id from token
+            const userId = decodedToken.userId
+
+            // fetch the user details from the databse
+            const user = await User.findById(userId).select('-password -__v');
+
+            response.status(200).json({message: "User deails fetched successfuily!", user})
+
+        }catch(e) {
+            return response.status(500).json({message: "error feaching user details. Try again later!", error:e.message})
         }
     }
 }
--- router/authRouter.js
+++ router/authRouter.js
@@ -1,11 +1,13 @@
 const express = require("express");
-const { register } = require("../controllers/authController");
+const { register, login, me } = require("../controllers/authController");
 
 
 // ctrate the router object
 const authRouter = express.Router();
 
-authRouter.post('/register', register)
+authRouter.post('/register', register);
+authRouter.post('/login', login);
+authRouter.get('/me', me)
 
 // expoer the router
 module.exports = authRouter;
